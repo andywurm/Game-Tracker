047b6635ab81889cef6c9b39dab9de8e32c59f97
andywurm <andaiyewurm@gmail.com>
2024-06-17 22:45:33 -0400

    Added Featured Games

--- src/Pages/LandingPage/LandingPage.tsx
+++ src/Pages/LandingPage/LandingPage.tsx
@@ -1,7 +1,24 @@
-import '../../FeaturedList'
+import React from 'react'
+import featuredGames from '../../FeaturedList'
 import './LandingPage.css'
 
 const LandingPage = () => {
-    return<div>Landing Page</div>
+
+    const [currentSlide, setCurrentSlide] = React.useState(1)
+
+    return <div className='landingPageContainer'>
+
+        <div className='featuredContainer'>
+            <div className='fGradient'>
+                <div className='fBackContainer'>
+                    <div className='fBlank'></div>
+                    <div className='fMainIMG'>
+                        <img src={featuredGames[currentSlide].mainIMG} className='mainIMG' />
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
 }
 export default LandingPage;
