791e15004de5f7b2ce974265fcfc72d388d33586
andywurm <andaiyewurm@gmail.com>
2024-06-21 22:44:23 -0400

    Added NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css';
-import React, {lazy} from 'react';
+import React, { lazy } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import NavBar from './Components/NavBar/NavBar';
 
-const Landing = lazy(() => import('./Pages/LandingPage/LandingPage')) 
-const GameClicked = lazy(() => import('./Pages/GameClickedPage/GameClicked')) 
+const Landing = lazy(() => import('./Pages/LandingPage/LandingPage'))
+const GameClicked = lazy(() => import('./Pages/GameClickedPage/GameClicked'))
 
 function App() {
 
@@ -11,18 +12,19 @@ function App() {
 
   React.useEffect(() => {
     fetch('http://localhost:8000/api/v1/games/')
-    .then((res) => res.json())
-    .then((data)  => setGames(data))
-  },[])
+      .then((res) => res.json())
+      .then((data) => setGames(data))
+  }, [])
 
   return (
     <div className="App">
       <BrowserRouter>
+        <NavBar/>
           <Routes>
-            <Route path="/" element={<Landing/>} />
-            <Route path="/GameClicked" element={<GameClicked/>} />
+            <Route path="/" element={<Landing />} />
+            <Route path="/GameClicked" element={<GameClicked />} />
           </Routes>
-        </BrowserRouter>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Pages/LandingPage/LandingPage.tsx
+++ src/Pages/LandingPage/LandingPage.tsx
@@ -5,48 +5,43 @@ import './LandingPage.css'
 const LandingPage = () => {
 
     const [currentSlide, setCurrentSlide] = React.useState(0)
-
-    const nextSlide = () => {
-        if(currentSlide === featuredGames.length-1){
-            setCurrentSlide(0)
-        }
-        else{
-            setCurrentSlide(currentSlide+1)
-        }
-    }
-
-    setInterval(nextSlide, 10000)
+    const [featGames, setFeatGames] = React.useState(featuredGames)
 
     React.useEffect(() => {
-        setInterval(nextSlide, 10000)
-    }, [])
+        const timerId = setInterval(
+            () =>
+                setCurrentSlide(() =>
+                    currentSlide === featGames.length-1 ? 0 : currentSlide + 1
+                ),
+            10000
+        );
+        return () => clearInterval(timerId);  
+    }, [currentSlide]);
 
-console.log(currentSlide)
+    return <div className='landingPageContainer'>
 
-return <div className='landingPageContainer'>
+        <div className='featuredContainer'>
 
-    <div className='featuredContainer'>
-
-        <div className='fGameInfo'>
-            <div className='gameLogo'>
-                <img src={featuredGames[currentSlide].titleIMG} className='titleIMG' />
+            <div className='fGameInfo'>
+                <div className='gameLogo'>
+                    <img src={featGames[currentSlide].titleIMG} className='titleIMG' />
+                </div>
+                <div className='hook'>{featGames[currentSlide].hook}</div>
+                <div className='summary'>{featGames[currentSlide].summary}</div>
+                <div className='readMore'><button className='readMoreBtn' onClick={() => console.log("clicked")}>Read More</button></div>
             </div>
-            <div className='hook'>{featuredGames[currentSlide].hook}</div>
-            <div className='summary'>{featuredGames[currentSlide].summary}</div>
-            <div className='readMore'><button className='readMoreBtn' onClick={() => console.log("clicked")}>Read More</button></div>
-        </div>
 
-        <div className='fGradient'>
-            <div className='fBackContainer'>
-                <div className='fBlank'></div>
-                <div className='fMainIMG'>
-                    <img src={featuredGames[currentSlide].mainIMG} className='mainIMG' />
+            <div className='fGradient'>
+                <div className='fBackContainer'>
+                    <div className='fBlank'></div>
+                    <div className='fMainIMG'>
+                        <img src={featGames[currentSlide].mainIMG} className='mainIMG' />
+                    </div>
                 </div>
             </div>
+
         </div>
 
     </div>
-
-</div>
 }
 export default LandingPage;
