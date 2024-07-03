ee1678d2dbc60f4115d7510367cb395c31bd654b
andywurm <andaiyewurm@gmail.com>
2024-07-02 22:19:51 -0400

    Switched to Local DB

--- API/db.js
+++ API/db.js
@@ -1,12 +1,11 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: process.env.REACT_APP_BASE_URL_USERNAME,
-    host: process.env.REACT_APP_BASE_URL_HOST,
-    database: process.env.REACT_APP_BASE_URL_DATABASE,
-    password: process.env.REACT_APP_BASE_URL_PASSWORD,
+    host: 'localhost',
+    database: 'games',
+    password: 'Andaiye1',
     port: 5432,
-    ssl: true
+    ssl: false
 });
 
 module.exports = pool;
