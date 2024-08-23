76fc7b9aca86ae7e7021bd917b5bf2ffeb07e3a2
andywurm <andaiyewurm@gmail.com>
2024-08-22 22:31:43 -0400

    Added Another Game Field

--- API/src/Games/queries.js
+++ API/src/Games/queries.js
@@ -3,7 +3,7 @@ const getGameByID = 'SELECT * FROM games WHERE ID=$1';
 const getGameByTitle = `SELECT * FROM games WHERE UPPER(title) LIKE UPPER($1)`;
 const getCompletedList = 'SELECT * FROM games WHERE id IN (SELECT gameid AS id FROM games_list WHERE listid=$1)'
 const checkGameExists = 'SELECT g FROM games  g WHERE g.title=$1';
-const addGame = 'INSERT INTO games (title, imgs, horizontalcover, verticalcover, releasedate, summary, platforms, genres, rating, contentrating) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)';
+const addGame = 'INSERT INTO games (title, imgs, horizontalcover, verticalcover, releasedate, summary, platforms, genres, rating, contentrating, othername) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)';
 const deleteGame = 'DELETE FROM games WHERE ID=$1'
 const updateGameReleaseDate = "UPDATE games SET releasedate=$1 WHERE ID=$2"
 
